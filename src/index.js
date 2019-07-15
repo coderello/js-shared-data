63e164404229d542657dba428b8d7f3d7a0fe25d
Ilya Sakovich <me@hivokas.com>
2019-07-15 21:54:43 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import SharedData from './SharedData';
 
-const sharedData = new SharedData;
+const sharedData = new SharedData();
 
 const shared = (...args) => sharedData.get(...args);
 
-export {
-    shared,
-    sharedData,
-};
+export { shared, sharedData };
 
 export default shared;
