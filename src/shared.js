ae4ad2ae21302084aa65ccf6d982ded5a6d4c0f2
Illia Sakovich <me@hivokas.com>
2019-12-31 23:26:42 +0200

    wip

--- src/shared.js
+++ src/shared.js
@@ -1,5 +1,9 @@
 import get from 'lodash/get';
 
-export default (path = null, defaultValue = null, namespace = get(window, 'sharedDataNamespace', 'sharedData')) => {
+export default (
+    path = null,
+    defaultValue = null,
+    namespace = get(window, 'sharedDataNamespace', 'sharedData')
+) => {
     return get(window, `${namespace}${path !== null ? `.${path}` : ''}`, defaultValue);
 };
